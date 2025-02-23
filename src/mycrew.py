c7dcacfa5345aeb3e7c5a6c9d8db75dc19bae52f
reicht <reicht@E010-MB004-WL-2.local>
2025-02-23 20:27:31 +0100

    write output of tasks to file

--- src/mycrew.py
+++ src/mycrew.py
@@ -4,30 +4,39 @@ from crewai.agents.crew_agent_executor import ToolResult
 from crewai.agents.parser import AgentAction, AgentFinish
 from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults #uv pip install langchain_community duckduckgo-search
 from crewai.tools import BaseTool
+from datetime import datetime
+import os.path
+from igi_helper import sanitize_filename
 
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 class myDuckDuckGoSearchTool(BaseTool):
-    name: str = "DuckDuckGo Search"
-    description: str = "Useful for finding up-to-date information from the web."
+	name: str = "DuckDuckGo Search"
+	description: str = "Useful for finding up-to-date information from the web."
+	
 
-    def _run(self, query: str) -> str:
-            # Ensure the DuckDuckGoSearchRun is invoked properly.
-            duckduckgo_tool = DuckDuckGoSearchRun()
-            response = duckduckgo_tool.invoke(query)
-            print(f"DDG_Response: {response}")
-            return response
-    	
-    def _get_tool(self):
-        # Create an instance of the tool when needed
-        return myDuckDuckGoSearchTool()
+	def _run(self, query: str) -> str:
+		# Ensure the DuckDuckGoSearchRun is invoked properly.
+		duckduckgo_tool = DuckDuckGoSearchRun()
+		# check if query has nested queries
+		print(f"\nDDGO-query:{query}\n")
+		response = duckduckgo_tool.invoke(query)
+		print(f"DDG_Response: {response}")
+		return response
+
+	def _get_tool(self):
+		# Create an instance of the tool when needed
+		return myDuckDuckGoSearchTool()
 
 
 @CrewBase
 class Mytestcrewa1():
 	"""Mytestcrewa1 crew"""
+
+	# additional info for file name outputs
+	ts = datetime.now()
 
 	# Learn more about YAML configuration files here:
 	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
@@ -58,10 +67,15 @@ class Mytestcrewa1():
 			print(f"agent finish: \nCLBK_REP_Finish_Thought: {output.thought}\nCLBK_REP_Finish_Output: {output.output}\nCLBK_REP_Finish_Text: {output.text}")
 		#if agent finish or agent action or Toolresult
 	try:
-		myllm_llama3_8b = LLM(api_key="fsdf", model="openai/meta-llama-3.1-8b-instruct",  base_url="http://localhost:1234/v1", temperature=0.7)
+		# myllm_llama3_8b = LLM(api_key="fsdf", model="openai/meta-llama-3.1-8b-instruct",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=12000)
+		myllm_llama3_8b = LLM(api_key="fsdf", model="openai/meta-llama-3-8b-instruct",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=12000)
 		# myllm_gemma2 = LLM(api_key="fsdf", model="openai/gemma-2-9b-it-8bit",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=0)
-		# myllm_gemma2 = LLM(api_key="fsdf", model="openai/gemma-2-27b",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=0)
-		myllm_r1_d_qwen = LLM(api_key="fsdf", model="openai/deepseek-r1-distill-qwen-14b",  base_url="http://localhost:1234/v1", temperature=0.0)
+		myllm_gemma2 = LLM(api_key="fsdf", model="openai/gemma-2-27b",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=0)
+		myllm_r1_d_qwen = LLM(api_key="fsdf", model="openai/deepseek-r1-distill-qwen-32b-mlx",  base_url="http://localhost:1234/v1", temperature=0.4, max_tokens=12000)
+		myllm_r1_d_llama = LLM(api_key="fsdf", model="openai/deepseek-r1-distill-llama-8b",  base_url="http://localhost:1234/v1", temperature=0.4, max_tokens=12000)
+		# myllm_mixtral = LLM(api_key="fsdf", model="openai/mixtral-8x7b-instruct-v0.1",  base_url="http://localhost:1234/v1", temperature=0.1, max_tokens=18000)
+		myllm_mixtral = LLM(api_key="fsdf", model="openai/mistral-small-24b-instruct-2501",  base_url="http://localhost:1234/v1", temperature=0.7, max_tokens=18000)
+		myllm_commandr = LLM(api_key="fsdf", model="openai/c4ai-command-r-v01@q8_0",  base_url="http://localhost:1234/v1", temperature=0.1, max_tokens=18000)
 		
 		# llm = LLM(api_key="fsdf", model="openai/deepseek-r1-distill-qwen-32b-mlx",  base_url="http://localhost:1234/v1", temperature=0.7)
 		# llm=LLM(model="ollama/llama3.2:latest", base_url="http://localhost:11434")
@@ -71,14 +85,29 @@ class Mytestcrewa1():
 		
 	# If you would like to add tools to your agents, you can learn more about it here:
 	# https://docs.crewai.com/concepts/agents#agent-tools
+	
+
+	@agent
+	def prompt_engineer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['prompt_engineer'],
+			verbose=True,
+			# step_callback=self.my_researcher_stepCallback,
+			# tools=[myDuckDuckGoSearchTool()],
+			llm=self.myllm_mixtral
+			# llm=self.myllm_llama3_8b
+		)
+
+
 	@agent
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
 			verbose=True,
-			step_callback=self.my_researcher_stepCallback,
+			# step_callback=self.my_researcher_stepCallback,
 			tools=[myDuckDuckGoSearchTool()],
-			llm=self.myllm_r1_d_qwen
+			# llm=self.myllm_r1_d_llama
+			llm=self.myllm_llama3_8b
 		)
 
 	@agent
@@ -86,26 +115,38 @@ class Mytestcrewa1():
 		return Agent(
 			config=self.agents_config['reporting_analyst'],
 			verbose=True,
-			step_callback=self.my_reporter_stepCallback,
-			tools=[myDuckDuckGoSearchTool()],
-			llm=self.myllm_llama3_8b
+			# step_callback=self.my_reporter_stepCallback,
+			# tools=[myDuckDuckGoSearchTool()],
+			llm=self.myllm_mixtral
 		)
+
 
 	# To learn more about structured task outputs, 
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
 
 	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+	def formulate_prompt_task(self) -> Task:
+		myTask=Task(
+		config=self.tasks_config['formulate_prompt_task'],
 		)
+		myTask.output_file=os.path.join('output',f"{self.generateFileName(myTask)}.md")
+		return myTask
+
+	# @task
+	def research_task(self) -> Task:
+		myTask=Task(
+			config=self.tasks_config['research_task'],
+		)
+		myTask.output_file=os.path.join('output',f"{self.generateFileName(myTask)}.md")
+		return myTask
+
+	# @task
+	def reporting_task(self) -> Task:
+		myTask = Task(
+			config=self.tasks_config['reporting_task'],
+		)
+		myTask.output_file=os.path.join('output',f"{self.generateFileName(myTask)}.md")
 
 	@crew
 	def crew(self) -> Crew:
@@ -120,3 +161,6 @@ class Mytestcrewa1():
 			verbose=True,
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
+
+	def generateFileName(self, myTask:Task):	
+		return sanitize_filename(f'task_{myTask.name}-model_{myTask.agent.llm.model}_temp_{myTask.agent.llm.temperature}_ts_{self.ts}')
