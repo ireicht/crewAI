a13d81639cc7ad15f9c2c4b448cebfa6656a90c9
Ignaz Reicht <contact@reicht.pro>
2026-01-20 18:28:02 +0100

    fix: run benchmark flag set feat: add all other parameters from the task setup to the benchmark result overview

--- src/benchmark_empty_timeout.py
+++ src/benchmark_empty_timeout.py
@@ -25,7 +25,7 @@ current_dir_path = os.path.dirname(current_file_path)
 # ToDo CONFIG: setup working dir and adjust paths accordingly
 working_dir = current_dir_path
 
-do_only_call_summarize = True
+do_only_call_summarize = False
 
 # Define the number of iterations
 iterations = 10
